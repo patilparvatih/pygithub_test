28fba38e3b99a79c7cdd84a04d23c3d36c75d5c9
Mallikarjun <165899099+patilparvatih@users.noreply.github.com>
2025-08-24 18:24:30 +0530

    Create Input.jsx
    Created a functional React Input component

---
+++ components/Input.jsx
@@ -0,0 +1,8 @@
+export default function Input(props){
+    return(
+        <>
+            <input style={{ color : "white" , fontSize:"20px", 
+                borderRadius : "10px" , padding : "10px" }}/>
+        </>
+    )
+}
